977e69955b2e107548daea9ff80708f085f7efb4
3rnestocs <3rnestocs@gmail.com>
2024-10-05 14:14:13 -0400

    Updated theme.

--- src/routes/institutional/Institutional.js
+++ src/routes/institutional/Institutional.js
@@ -1,15 +1,19 @@
-// src/pages/institutional/Institutional.js
 import React from 'react';
 import { Typography, Button } from '@mui/material';
 
 const Institutional = () => {
   return (
     <div>
-      <Typography variant="h1">Bienvenido al Banco Universitario</Typography>
+      <Typography variant="h3">Bienvenido al Banco Universitario</Typography>
       <Typography variant="body1">
         Estamos comprometidos para brindarte los mejores servicios!...
       </Typography>
-      <Button variant="contained" color="primary">Iniciar sesión</Button>
+      <Button variant="contained" color="primary">
+        Iniciar sesión
+      </Button>
+      <Button variant="text" color="primary">
+        Hacer otra cosa
+      </Button>
     </div>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,48 @@
 // src/theme.js
 import { createTheme } from '@mui/material/styles';
 
-// Create a theme instance
 const theme = createTheme({
   typography: {
-    fontFamily: 'Roboto, sans-serif', // Default font for body text
+    fontFamily: 'Roboto, sans-serif',
     h1: {
-      fontFamily: 'Roboto, sans-serif', // For titles
+      fontFamily: 'Roboto, sans-serif',
     },
     h2: {
-      fontFamily: 'Roboto, sans-serif', // For titles
+    fontFamily: 'Roboto, sans-serif', // For titles
     },
     h3: {
-      fontFamily: 'Roboto, sans-serif', // For titles
+    fontFamily: 'Roboto, sans-serif', // For titles
     },
     button: {
-      fontFamily: 'Montserrat Alternates, sans-serif', // For buttons
+      fontFamily: 'Montserrat Alternates, sans-serif',
     },
-    subtitle1: {
-      fontFamily: 'Montserrat Alternates, sans-serif', // For other texts like input labels, etc.
-    },
-    body1: {
-      fontFamily: 'Roboto, sans-serif', // Normal body text
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px', // Optional: You can customize this
+          textTransform: 'none', // Optional: To remove uppercase styling
+        },
+        // Button with background (primary button)
+        containedPrimary: {
+          backgroundColor: '#49BEB7',
+          color: '#fff',
+          fontFamily: 'Roboto, sans-serif',
+          '&:hover': {
+            backgroundColor: '#39a59f', // Adjust hover color
+          },
+        },
+        // Button without background (text button)
+        textPrimary: {
+          color: '#085F63',
+          fontFamily: 'Montserrat Alternates, sans-serif',
+          '&:hover': {
+            backgroundColor: 'transparent', // No background on hover
+            color: '#046060', // Darker hover color
+          },
+        },
+      },
     },
   },
 });
