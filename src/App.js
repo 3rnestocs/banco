f2d72836bc2a8d3ea4237eb23fc7f1b7a7c0a109
3rnestocs <3rnestocs@gmail.com>
2024-10-05 13:31:21 -0400

    HashRouter renamed properly.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import { Routes, Route} from "react-router-dom";
 import Institutional from "./routes/institutional/Institutional";
 import Login from "./routes/login/Login";
-// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Institutional />} />
         <Route path="/login" element={<Login />} />
