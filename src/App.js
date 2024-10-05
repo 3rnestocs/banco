e651c4e921e10c2ef31bda26f461986c33d6517a
3rnestocs <3rnestocs@gmail.com>
2024-10-05 14:03:02 -0400

    Updated missing app.js file.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'; // Optional: For Material UI reset
+import theme from './theme'; // Import the custom theme
 import { Routes, Route} from "react-router-dom";
 import Institutional from "./routes/institutional/Institutional";
 import Login from "./routes/login/Login";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+    <CssBaseline /> {/* Optional: For consistent reset of global styles */}
       <Routes>
         <Route path="/" element={<Institutional />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </>
+      </ThemeProvider>
   );
 }
 export default App;
