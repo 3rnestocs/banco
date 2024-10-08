9cf0a3756523a950a5ca36b90d555150578cc176
3rnestocs <3rnestocs@gmail.com>
2024-10-08 12:56:13 -0400

    Fix on dynamicroutes import.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
-import DynamicRoutes from './DynamicRoutes'; // Import the component
+import DynamicRoutes from './components/DynamicRoutes';
 
 function App() {
   return (
